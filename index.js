4ac57f743689f991397a3ec88367620d7be60005
VitorSantosH <wytorh@gmail.com>
2023-07-26 20:50:51 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,114 +1,44 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const app = express();
-const port = 5000
-const fs = require('fs')
+
+var certificade
+try {
+    certificade = {
+        key: fs.readFileSync("/etc/letsencrypt/live/vitorwebdev.com.br/privkey.pem", 'utf8'),
+        cert: fs.readFileSync("/etc/letsencrypt/live/vitorwebdev.com.br/fullchain.pem", 'utf8')
+    };
+
+} catch (error) {
+    console.log(error)
+}
 
 
-const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('common'));
 
-const obj = []
+app.use('/', express.static('dist'))
+const portHttp = 8080;
+const portHttpS = 8443
+const httpsServer = https.createServer(certificade, app);
+const httpServer = http.createServer(app);
 
-obj[0] = {
-    name: "A história",
-    span: "Lorem Ipsium larium das rerum novarum et das enciclicasr epsium larium et ipsis litteris",
-    date: Date.now(),
-    thumbnail: "maxresdefault.jpg",
-    URL: 'COMANDOTORRENTS.COM.mp4'
+
+httpServer.listen(portHttp, function () {
+    console.log("JSON Server is running on " + portHttp);
+});
+
+try {
+    httpsServer.listen(portHttpS, function () {
+        console.log('Second site is running on port ' + portHttpS);
+    });
+} catch (error) {
+    console.log(error)
 }
 
-obj.push({
-    name: "Chvrched",
-    span: "Lorem Ipsium larium das rerum novarum et das enciclicasr epsium larium et ipsis litteris",
-    date: Date.now(),
-    thumbnail: "star-trek-beyond-blu-ray-deleted-scene.jpg",
-    URL: '01. Introdução ao ASPNET Core.mp4'
-})
-
-app.use('/static', express.static('assets'))
-
-app.use('/video/:id', async (req, res ) => {
-
-    const video =  obj.filter(item => {
-        console.log(item.name)
-        console.log(req.params.id)
-        if(item.name == req.params.id) return item
-    })
-
-    res.send({video})
-})
-
-app.get('/feed', (req, res ) => {
-
-    res.send(obj)
-})
-//app.use('/teste', express.static('index.html'))
-app.use('/', express.static('build') )
-app.use('/login', (req, res ) =>{
-    console.log(req.body)
-    const erro = []
-    const user = {
-        name: "João das Couves",
-        token: "xxxx"
-    }
-
-    if(req.body.email !== 'teste') {
-        erro.push('user')
-    }
-
-    if(req.body.password !== '12345'){
-        erro.push('senha')
-    }
-
-    if(erro.length > 0){
-        return  res.send({erros: erro})
-    }
-
-    res.send(user)
-})
-
-
-
-app.listen(port, () => {
-    console.log(`Servidor funcionando na porta ${port}`)
-})
-
-
-
-
-/**
- * 
- * 
-app.use('/video/:id', (req, res ) => {
-
-    const range = req.headers.range || 0;
-
-    const videoPath = `${__dirname}/assets/${req.params.id}`;
-    const videoSize = fs.statSync(`${__dirname}/assets/${req.params.id}`).size;
-    const CHUNK_SIZE = 10 ** 6; // 1mb
-    const start =  0 // Number(range.replace(/\D/g,""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-
-    const contendLenghth = end - start + 1;
-
-    const headers = {
-        "Content-Range" : `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges" : "bytes",
-        "Content-Length": contendLenghth,
-        "Content-Type": "video/mp4",
-
-    }
-
-    res.header = {...headers}
-    res.status(206);
-    const videoStream = fs.createReadStream(videoPath, {start :  start, end : end });
-
-    videoStream.pipe(res)
-
-})
- * 
- * 
- * 
- */
